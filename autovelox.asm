5c09591fcc81f241d3257a05b0920c6e7f32bcfc
Andrea Montemurro <andrea.montemurro97@gmail.com>
2017-05-25 23:40:47 +0200

    Update autovelox.asm

--- autovelox.asm
+++ autovelox.asm
@@ -6,10 +6,11 @@
 
 .text
 
-INIZIO:	addi $t8, $zero, 62500000	#attesa 25 ms
-		addi $t9, $zero, 250000000	#attesa 1 s
-		add $s0, $zero, $zero		#primo VAL calcola cicli 								di cpu per misurare
-							temp di sett a 1 c. 7e6
+main:		li $t7, 62500000			#attesa 25 ms nel r.temp
+		add $t8, $zero, $t7		#attesa 25 ms nel reg
+		li $t7, 250000000	
+		add $t9, $zero, $t7		#attesa 1 s
+		add $s0, $zero, $zero		#primo VAL calcola ciclidicpu per misurare temp di sett a 1 c. 7e6
 
 PRIMAC:	lb $s1, 0x10010000		#s1 cont. il byte IN 
 		andi $t1, $s1, 128		#essendo 1 aposto 7,2^7
@@ -20,7 +21,7 @@ SECONDAC:	lb $s1, 0x10010000
 		andi $t1, $s1, 192
 		beq $t1, $zero, SECONDAC
 
-		shll $s0,2				#tempo*4
+		sll $s0,$s0,2				#tempo*4
 		
 		lw $s2, 0x10010010		#legge word con freq cl
 		div $s0, $s2
@@ -36,24 +37,38 @@ SECONDAC:	lb $s1, 0x10010000
 		bne $t3, $zero CASO2		#CASO2 50<vel<55
 
 		slti $t3, $s2, 60
-		bne $t3, $zero CASO3		#CASO2 55<vel<60
+		bne $t3, $zero CASO3		#CASO3 55<vel<60
 
 CASO4:	addi $t9, $t9, -1		#CASO4 vel>60
 		bne $t9, $zero, CASO4
-		addi $s3, $zero, 56		#56=2^3+2^4+2^5 cellea 1			addi $s4, $zero, 0x1001008	#SCRIVO OUT  					sb $s3, 0($s4)			#s3 contiene valore cell			jump FINE
+		addi $s3, $zero, 56		#56=2^3+2^4+2^5 cellea 1			
+		li $t7, 0x10010001
+		add $s4, $zero, $t7		#SCRIVO OUT  					
+		sb $s3, 0($s4)			#s3 contiene valore cell			
+		j FINE
 
-CASO1:	jump FINE				#nessuna infrazione
+CASO1:	j FINE				#nessuna infrazione
 
-CASO2:	addi $t9, $t9, -1		#CASO4 vel>60
+CASO2:	addi $t9, $t9, -1		#CASO2 50<vel<55
 		bne $t9, $zero, CASO2
-		addi $s3, $zero, 24		#24=2^3+2^4 cellea 1				addi $s4, $zero, 0x1001008	#SCRIVO OUT  					sb $s3, 0($s4)			#s3 contiene valore cell			jump FINE
+		addi $s3, $zero, 24		#24=2^3+2^4 cellea 1				
+		li $t7, 0x10010001
+		add $s4, $zero, $t7		#SCRIVO OUT  					
+		sb $s3, 0($s4)			#s3 contiene valore cell			
+		j FINE
 
-CASO3:	addi $t9, $t9, -1		#CASO4 vel>60
+CASO3:	addi $t9, $t9, -1		#CASO3 55<vel<60
 		bne $t9, $zero, CASO3
-		addi $s3, $zero, 40		#40=2^3+2^5 cellea 1				addi $s4, $zero, 0x1001008	#SCRIVO OUT  					sb $s3, 0($s4)			#s3 contiene valore cell			jump FINE
+		addi $s3, $zero, 40		#40=2^3+2^5 cellea 1
+
+		li $t7, 0x10010001
+		add $s4, $zero, $t7	#SCRIVO OUT  					
+		sb $s3, 0($s4)			#s3 contiene valore cell			
+		j FINE
 
 FINE:		addi $t8, $t8, -1		#attesa 25 ms 
-		bne $t8, $zero, INIZIO	#ricontrolla segmento
+		bne $t8, $zero, FINE		#ricontrolla segmento
+		j main
 
 	
 
