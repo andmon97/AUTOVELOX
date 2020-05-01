5144e7d7a46c41ab14fe5f7f554673ec8f04a598
Andrea Montemurro <andrea.montemurro97@gmail.com>
2020-05-01 18:48:16 +0200

    Update autovelox.asm

--- autovelox.asm
+++ autovelox.asm
@@ -12,7 +12,7 @@ main:		li $t7, 62500000		#attesa 25 ms nel r.temp
 		add $t9, $zero, $t7		#attesa 1 s
 		add $s0, $zero, $zero		#primo VAL calcola ciclidicpu per misurare temp di sett a 1 c. 7e6
 
-PRIMAC:	lb $s1, 0x10010000			#s1 cont. il byte IN 
+PRIMAC:		lb $s1, 0x10010000		#s1 cont. il byte IN 
 		andi $t1, $s1, 128		#essendo 1 aposto 7,2^7
 		beq $t1, $zero PRIMAC
 
@@ -39,7 +39,7 @@ SECONDAC:	lb $s1, 0x10010000
 		slti $t3, $s2, 60
 		bne $t3, $zero CASO3		#CASO3 55<vel<60
 
-CASO4:	addi $t9, $t9, -1			#CASO4 vel>60
+CASO4:		addi $t9, $t9, -1		#CASO4 vel>60
 		bne $t9, $zero, CASO4
 		addi $s3, $zero, 56		#56=2^3+2^4+2^5 cellea 1			
 		li $t7, 0x10010001
@@ -47,9 +47,9 @@ CASO4:	addi $t9, $t9, -1			#CASO4 vel>60
 		sb $s3, 0($s4)			#s3 contiene valore cell			
 		j FINE
 
-CASO1:	j FINE					#nessuna infrazione
+CASO1:		j FINE				#nessuna infrazione
 
-CASO2:	addi $t9, $t9, -1			#CASO2 50<vel<55
+CASO2:		addi $t9, $t9, -1		#CASO2 50<vel<55
 		bne $t9, $zero, CASO2
 		addi $s3, $zero, 24		#24=2^3+2^4 cellea 1				
 		li $t7, 0x10010001
@@ -57,7 +57,7 @@ CASO2:	addi $t9, $t9, -1			#CASO2 50<vel<55
 		sb $s3, 0($s4)			#s3 contiene valore cell			
 		j FINE
 
-CASO3:	addi $t9, $t9, -1			#CASO3 55<vel<60
+CASO3:		addi $t9, $t9, -1		#CASO3 55<vel<60
 		bne $t9, $zero, CASO3
 		addi $s3, $zero, 40		#40=2^3+2^5 cellea 1
 
